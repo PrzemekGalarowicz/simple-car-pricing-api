85a089e0a136ef96ef914e561df2a6854d6e4e06
Przemysław Galarowicz <pgalarowicz@gmail.com>
2023-12-27 14:09:29 +0100

    chore: preprod part 1

--- src/app.module.ts
+++ src/app.module.ts
@@ -38,12 +38,14 @@ import cookieSession from 'cookie-session';
   ],
 })
 export class AppModule {
+  constructor(private configService: ConfigService) {}
+
   configure(consumer: MiddlewareConsumer) {
     consumer
       .apply(
         cookieSession({
           name: 'session',
-          keys: ['k'],
+          keys: [this.configService.get('COOKIE_KEY')],
         }),
       )
       .forRoutes('*');
